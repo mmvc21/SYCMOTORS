ae3ea8782b2337a284624dbd917c0fae13df2f0f
mmvc21 <mmvc.0421@gmail.com>
2020-08-17 11:19:35 -0500

    8/17/20

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<meta charset="utf-8">
+<head>
+	<title>Prospecta</title>
+</head>
+<body>
+<img src="Img/Logo.png">
+<ul>
+	<li><label for='Usuario'>Usuario</label></li>
+	<li><input type="text" name="Usuario" placeholder="Introduce Tu Usuario"></li>
+	<li><label for='Pass'>Contraseña</label></li>
+	<li><input type="text" name="Pass" placeholder="Introduce Tu Contraseña"></li>
+</ul>
+<img src="Img/Banner/Banner1.jpg">
+<h2>Registro</h2>
+<p>
+<label for="Nombre">Nombre</label>
+<input type="text" name="Nombre" placeholder="Introduce tu Nombre">
+</p>
+<p>
+<label for="APP">Apellido Paterno</label>
+<input type="text" name="APP" placeholder="Introduce tu Apellido Paterno">
+</p>
+</body>
+</html>
